e4b595a6d6fea5a8e3c0fc0d3891ff0067eaaa4d
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-11-18 15:15:48 -0800

    Created menu class
    Displays generic CLI menu

---
+++ dcc/src/main/java/com/paydues/Menu.java
@@ -0,0 +1,21 @@
+package com.paydues;
+
+public class Menu {
+    // Prints big line of #
+    protected static void bigLine(){
+        System.out.println("########################################");
+    }
+    // Prints the message
+    protected static void rules(){
+        System.out.println("#   Welcome to the Probability Club    #");
+        System.out.println("#                                      #");
+        System.out.println("# Dues are established by playing one  #");
+        System.out.println("# of two games                         #");
+        System.out.println("#                                      #");
+        System.out.println("# Game One: Roll a 5 followed by a 6   #");
+        System.out.println("# Game One: Roll a 5 followed by a 5   #");
+        System.out.println("#                                      #");
+        System.out.println("# Dues are $1 per 1 roll               #");
+    }
+
+}
