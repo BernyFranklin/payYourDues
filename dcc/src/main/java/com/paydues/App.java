ad98de13410a60d4ceb4074da4bc5396f1f98cb3
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-11-18 14:18:01 -0800

    Documented prompt for program

--- dcc/src/main/java/com/paydues/App.java
+++ dcc/src/main/java/com/paydues/App.java
@@ -1,7 +1,20 @@
 package com.paydues;
 
 /**
- * Hello world!
+ * Alice wants to join her school's Probability Student 
+ * Club. Membership dues are computed via one of two 
+ * simple probabilistic games.
+ * 
+ * The first game: roll a die repeatedly. Stop rolling once
+ * you get a 5 followed by a 6. Your number of rolls is
+ * the amount you pay in dollars.
+ * 
+ * The second game: same, except that the stopping 
+ * condition is a 5 followed by a 5.
+ * 
+ * Which of the two games should Alice elect to play?
+ * Does it even matter? Write a program to simulate the 
+ * two games and calculate their expected value.
  *
  */
 public class App 
